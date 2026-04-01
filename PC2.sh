1854632971c2a8db15457ecc8ae406205ffabd87
Syeeb25 <malliksyeeb@gmail.com>
2026-04-01 14:57:45 +0530

    Added config for PC2

---
+++ PC2.sh
@@ -0,0 +1,18 @@
+
+Executing the startup file
+
+
+PC2> ip 192.168.1.2 255.255.255.0 192.168.1.1
+Checking for duplicate address...
+PC2 : 192.168.1.2 255.255.255.0 gateway 192.168.1.1
+
+PC2> ping 10.0.0.1
+
+84 bytes from 10.0.0.1 icmp_seq=1 ttl=60 time=70.796 ms
+84 bytes from 10.0.0.1 icmp_seq=2 ttl=60 time=52.541 ms
+84 bytes from 10.0.0.1 icmp_seq=3 ttl=60 time=52.503 ms
+84 bytes from 10.0.0.1 icmp_seq=4 ttl=60 time=49.911 ms
+84 bytes from 10.0.0.1 icmp_seq=5 ttl=60 time=46.430 ms
+
+
+
